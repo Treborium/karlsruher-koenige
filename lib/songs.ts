dee7a22684d98776f1ede300924c639238fc292e
Robert Fuchs <robert.fuchs@logmein.com>
2021-05-20 22:40:31 +0200

    configure lint-staged

--- lib/songs.ts
+++ lib/songs.ts
@@ -1,15 +1,15 @@
 export interface Song {
   id: number;
   title: string;
-} 
+}
 
 export function getSongs(): Song[] {
   return [
-    {id: 1, title: 'Königslied'},
-    {id: 2, title: 'Schlachtruf'},
-    {id: 3, title: 'Kein Bier auf Hawaii'},
-    {id: 4, title: 'Song 3'},
-    {id: 5, title: 'Sonst noch was'},
-    {id: 6, title: 'etc.'},
+    { id: 1, title: 'Königslied' },
+    { id: 2, title: 'Schlachtruf' },
+    { id: 3, title: 'Kein Bier auf Hawaii' },
+    { id: 4, title: 'Song 3' },
+    { id: 5, title: 'Sonst noch was' },
+    { id: 6, title: 'etc.' },
   ];
 }
