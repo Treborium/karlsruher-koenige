4f5cb024b18ba62edfbf08dd6d2aad268208406f
Robert Fuchs <robert.fuchs@logmein.com>
2021-05-20 17:24:46 +0200

    create page for beer counter

---
+++ pages/beer.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+export default function Beer() {
+  return (
+    <Layout heading='Kasten Zähler'>
+    <h1>Site under construction. Please come back later</h1>
+    </Layout>
+  );
+}
