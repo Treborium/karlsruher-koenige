8e223f650b56d48ac5080abea51db0d20d12a4dc
Robert Fuchs <robert.fuchs@logmein.com>
2021-05-24 09:30:59 +0200

    add beer counter

--- pages/beer.tsx
+++ pages/beer.tsx
@@ -1,9 +1,18 @@
-import Layout from '../components/layout'
+import { useState } from 'react';
+import Layout from '../components/layout';
+import styles from '../styles/beer.module.scss';
 
 export default function Beer() {
+  const [count, setCount] = useState(0);
+
   return (
-    <Layout heading='Kasten Zähler'>
-    <h1>Site under construction. Please come back later</h1>
+    <Layout heading="Kasten Zähler">
+      <div className={styles.container}>
+        <div className={styles.counter}>{count}</div>
+        <button className={styles.button} onClick={() => setCount(count + 1)}>
+          Kasten!
+        </button>
+      </div>
     </Layout>
   );
 }
