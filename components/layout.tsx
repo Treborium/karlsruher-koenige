02bd32ca7c33ad2afe5d781ae4c752a79aae5918
Robert Fuchs <robert.fuchs@logmein.com>
2021-06-26 22:46:22 +0200

    use material-ui for top bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,6 @@
+import { AppBar, Typography, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 import styles from '../styles/layout.module.scss';
 import Menu from './menu';
 
@@ -6,17 +9,31 @@ export interface LayoutProps {
   children?: JSX.Element | JSX.Element[];
 }
 
+const useStyles = makeStyles({
+  appBar: {
+    flexGrow: 1,
+    padding: '2vh 0',
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
 export default function Layout(props: LayoutProps) {
+  const classes = useStyles();
+
   return (
-    <div className={styles.flexContainer} >
-      <div className={styles.header} >
-        <h1 className={styles.heading}>
-          {props.heading}
-        </h1>
-      </div>
-      <div className={styles.content}>
-        {props.children}
-      </div>
+    <div className={styles.flexContainer}>
+      <AppBar position="static" className={classes.appBar}>
+        <Grid container justify="center" alignItems="center">
+          <Grid item>
+            <Typography variant="h6" className={classes.title}>
+              {props.heading}
+            </Typography>
+          </Grid>
+        </Grid>
+      </AppBar>
+      <div className={styles.content}>{props.children}</div>
       <Menu />
     </div>
   );
