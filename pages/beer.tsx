d6e58c8f9e1755108cbe65cb6d4ffab367fd77ce
Robert Fuchs <robert.fuchs@logmein.com>
2021-06-26 20:53:45 +0200

    convert beer.tsx to use material-ui

--- pages/beer.tsx
+++ pages/beer.tsx
@@ -1,18 +1,45 @@
 import { useState } from 'react';
+import { Button, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Layout from '../components/layout';
 import styles from '../styles/beer.module.scss';
 
+const useStyles = makeStyles({
+  root: {
+    height: '90vh',
+  },
+});
+
 export default function Beer() {
+  const classes = useStyles();
   const [count, setCount] = useState(0);
 
   return (
     <Layout heading="Kasten Zähler">
-      <div className={styles.container}>
-        <div className={styles.counter}>{count}</div>
-        <button className={styles.button} onClick={() => setCount(count + 1)}>
-          Kasten!
-        </button>
-      </div>
+      <Grid
+        container
+        direction="column"
+        spacing={4}
+        alignItems="center"
+        justify="center"
+        className={classes.root}
+      >
+        <Grid item>
+          <Typography variant="h2">{count}</Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            className={styles.button}
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => setCount(count + 1)}
+          >
+            Kasten!
+          </Button>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
