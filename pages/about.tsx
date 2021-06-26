063fa1d5a9c93ceb60ed820b0edc36641b93088c
Robert Fuchs <robert.fuchs@logmein.com>
2021-06-26 20:29:47 +0200

    start converting about.tsx to material-ui

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,29 +1,185 @@
 import Layout from '../components/layout';
-import styles from '../styles/about.module.scss'
-import { ExternalLink, LogIn, Clock, Mail } from 'react-feather';
+import styles from '../styles/about.module.scss';
+
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Button,
+  Grid,
+} from '@material-ui/core';
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Mail from '@material-ui/icons/Mail';
+import Launch from '@material-ui/icons/Launch';
+import { FunctionComponent } from 'react';
 
 export default function About() {
+  const createData = (day: string, time: string, group: string) => ({
+    day,
+    time,
+    group,
+  });
+
+  const rows = [
+    createData('Montag', '20:30 Uhr', 'Alle'),
+    createData('Donnerstag', '20:00 Uhr', 'Alle'),
+    createData('Freitag', '17:30 Uhr', 'Wettkampf'),
+  ];
+
   return (
-    <Layout heading='Karlsruher Könige'>
+    <Layout heading="Karlsruher Könige">
       <h2 className={styles.heading}>Wer sind wir?</h2>
       <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua.
       </p>
 
-      <div className={styles.links}>
-        <a className={styles.link} href='https://buchsys.sport.uni-karlsruhe.de/angebote/aktueller_zeitraum/index.html'>
-          <LogIn className={styles.icon}/>
-          <li className={styles.listItem}>Anmeldung</li>
-        </a>
-        <a className={styles.link} href='https://buchsys.sport.uni-karlsruhe.de/angebote/aktueller_zeitraum/index.html'>
-          <Clock className={styles.icon}/>
-          <li className={styles.listItem}>Trainingszeiten</li>
-        </a>
-        <a className={styles.link} href='mailto:turnkoenige@gmail.com' target='_blank'>
-          <Mail className={styles.icon}/>
-          <li className={styles.listItem}>Kontakt</li>
-        </a>
-      </div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Anmeldung</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container direction="column" alignItems="center" spacing={2}>
+            <Grid item>
+              <Typography>
+                Du hast Interesse den Karlsruher Königen beizutreten und beim
+                Unisport teilzunehmen? Dann melde dich auf der
+                Hochschulsport-Webseite an und lass dich im nächsten Training
+                blicken!
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                href="https://buchsys.sport.uni-karlsruhe.de/angebote/aktueller_zeitraum/index.html"
+                endIcon={<Launch />}
+              >
+                Anmeldung
+              </Button>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Trainingszeiten</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container direction="column" alignItems="center" spacing={2}>
+            <Grid item>
+              <TableContainer component={Paper}>
+                <Table aria-label="simple table" size="small">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Tag</TableCell>
+                      <TableCell>Zeit</TableCell>
+                      <TableCell>Gruppe</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow key={row.day}>
+                        <TableCell>{row.day}</TableCell>
+                        <TableCell>{row.time}</TableCell>
+                        <TableCell>{row.group}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            <Grid item>
+              <Typography>
+                Bitte schaue auf die offizielle Hochschuleseite für die
+                aktuellsten Trainingszeiten
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                href="https://buchsys.sport.uni-karlsruhe.de/angebote/aktueller_zeitraum/index.html"
+              >
+                Trainingszeiten
+              </Button>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+      <AccordionWithButton
+        title="Kontakt"
+        buttonText="Mail Senden"
+        buttonLink="mailto:turnkoenige@gmail.com"
+      >
+        Falls du Fragen hast oder gerne Kontakt zu uns aufnehmen möchtest,
+        erreichst du uns am besten per Mail:
+      </AccordionWithButton>
     </Layout>
   );
 }
+
+interface AccordionWithButtonProps {
+  title: string;
+  buttonText: string;
+  buttonLink: string;
+}
+
+const AccordionWithButton: FunctionComponent<AccordionWithButtonProps> = ({
+  title,
+  buttonText,
+  buttonLink,
+  children,
+}) => {
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>{title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container direction="column" alignItems="center" spacing={2}>
+          <Grid item>
+            <Typography>{children}</Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              href={buttonLink}
+              startIcon={<Mail />}
+            >
+              {buttonText}
+            </Button>
+          </Grid>
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
