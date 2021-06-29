a450e5870c6606b6d5fa60d443f60983620a774d
Robert Fuchs <robert.fuchs@logmein.com>
2021-06-29 12:40:11 +0200

    center all pages and set max width

--- components/layout.tsx
+++ components/layout.tsx
@@ -20,6 +20,7 @@ const useStyles = makeStyles({
   },
   content: {
     minHeight: '100vh',
+    maxWidth: '800px',
     flexGrow: 1,
     padding: '1vh 7vw',
   },
@@ -29,15 +30,17 @@ export default function Layout(props: LayoutProps) {
   const classes = useStyles();
 
   return (
-    <Grid container direction="column">
-      <AppBar position="static" className={classes.appBar}>
-        <Grid container justify="center" alignItems="center">
+    <Grid container direction='column'>
+      <AppBar position='static' className={classes.appBar}>
+        <Grid container justify='center' alignItems='center'>
           <Grid item>
-            <Typography variant="h6">{props.heading}</Typography>
+            <Typography variant='h6'>{props.heading}</Typography>
           </Grid>
         </Grid>
       </AppBar>
-      <div className={classes.content}>{props.children}</div>
+      <Grid container justify='center'>
+        <div className={classes.content}>{props.children}</div>
+      </Grid>
       <Menu currentPage={props.currentPage} />
     </Grid>
   );
