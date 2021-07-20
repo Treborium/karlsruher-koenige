285d34bba0a27057f1c2463ee68a98820ecbbbf5
Robert Fuchs <robert.fuchs@logmein.com>
2021-07-20 22:15:52 +0200

    add menu icon to top bar

--- components/top-menu.tsx
+++ components/top-menu.tsx
@@ -1,16 +1,35 @@
-import { AppBar, Grid, Icon, IconButton, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Button,
+  Icon,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+  Toolbar,
+  Link,
+  ListItemText,
+  ListItemIcon,
+  Grid,
+} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 import Show from './show';
 
-const useStyles = makeStyles({
-  appBar: {
-    padding: '2vh 0',
-    marginBottom: '3vh',
-  },
-  backIcon: {
-    color: '#FFF',
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      marginBottom: '3vh',
+    },
+    backIcon: {
+      color: '#FFF',
+    },
+    heading: {
+      flexGrow: 1,
+      marginLeft: theme.spacing(2),
+    },
+  })
+);
 
 interface TopMenuProps {
   heading: string;
@@ -19,22 +38,68 @@ interface TopMenuProps {
 
 export default function TopMenu(props: TopMenuProps) {
   const classes = useStyles();
+  const [anchorElem, setAnchorElem] = useState<null | HTMLElement>(null);
   const navigateBack = () => history.back();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElem(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorElem(null);
+  };
 
   return (
     <AppBar position='static' className={classes.appBar}>
-      <Grid container justify='center' alignItems='center'>
+      <Toolbar>
         <Show when={props.showBackIcon}>
-          <Grid item className={classes.backIcon}>
-            <IconButton color='inherit' size='small' onClick={navigateBack}>
-              <Icon className='fas fa-arrow-left' />
-            </IconButton>
-          </Grid>
+          <IconButton
+            color='inherit'
+            size='small'
+            onClick={navigateBack}
+            className={classes.backIcon}
+          >
+            <Icon className='fas fa-arrow-left' />
+          </IconButton>
         </Show>
-        <Grid item>
-          <Typography variant='h6'>{props.heading}</Typography>
-        </Grid>
-      </Grid>
+
+        <Typography variant='h6' className={classes.heading}>
+          {props.heading}
+        </Typography>
+
+        <Button
+          aria-controls='simple-menu'
+          aria-haspopup='true'
+          size='small'
+          color='inherit'
+          onClick={handleClick}
+        >
+          <Icon className='fas fa-bars' />
+        </Button>
+        <Menu
+          id='simple-menu'
+          anchorEl={anchorElem}
+          keepMounted
+          open={Boolean(anchorElem)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <Grid container justify='space-between'>
+              <Link
+                underline='none'
+                color='textPrimary'
+                href='https://shop.spreadshirt.de/karlsruher-koenige'
+              >
+                Merch Shop
+              </Link>
+              {/* TODO: style icon properly */}
+              {/* <Icon className='fas fa-external-link-alt' /> */}
+            </Grid>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>Wanderkasten</MenuItem>
+          <MenuItem onClick={handleClose}>Impressum</MenuItem>
+        </Menu>
+      </Toolbar>
     </AppBar>
   );
 }
