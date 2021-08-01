af5a6b4c9f5dc04f2c911a68ff9cb6291b4e8b7c
Robert Fuchs <robert.fuchs@logmein.com>
2021-08-01 21:52:23 +0200

    add margin to account for bottom nav bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { loadCSS } from 'fg-loadcss';
 import { useEffect } from 'react';
 
@@ -14,17 +14,19 @@ export interface LayoutProps {
   children?: React.ReactNode;
 }
 
-const useStyles = makeStyles({
-  root: {
-    minHeight: '100vh',
-    width: '100vw',
-  },
-  content: {
-    maxWidth: '800px',
-    flexGrow: 1,
-    padding: (props: LayoutProps) => (props.noPadding ? 0 : '1vh 7vw'),
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100vw',
+      marginBottom: theme.spacing(10),
+    },
+    content: {
+      maxWidth: '800px',
+      flexGrow: 1,
+      padding: (props: LayoutProps) => (props.noPadding ? 0 : '1vh 7vw'),
+    },
+  })
+);
 
 export default function Layout(props: LayoutProps) {
   const classes = useStyles(props);
@@ -41,7 +43,7 @@ export default function Layout(props: LayoutProps) {
   }, []);
 
   return (
-    <Grid container direction='column'>
+    <Grid container direction='column' className={classes.root}>
       <TopMenu heading={props.heading} showBackIcon={props.showBackIcon} />
       <Grid container justify='center'>
         <div className={classes.content}>{props.children}</div>
